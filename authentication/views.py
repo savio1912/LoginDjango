189020331bb8bc809bfacfeb6440c92150fa9243
Savio <iamsavio19@gmail.com>
2023-01-26 23:19:19 +0530

    first commit

---
+++ authentication/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,"authentication/index.html")
+
+def signup(request):
+    return render(request, "authentication/signup.html")    
+
+def signin(request):
+    return render(request, "authentication/signin.html") 
+
+def signout(request):
+    pass
